99e20db21dcfe51d85063f78839367be056f8505
dimasikjitss <57629224+dimasikjitss@users.noreply.github.com>
2020-07-05 21:47:26 +0600

    Update screen.py

--- screen.py
+++ screen.py
@@ -1,15 +1,15 @@
-from selenium import webdriver   
+from selenium import webdriver 
+
 def screen_f(message):
     op = webdriver.ChromeOptions()
     op.add_argument('--headless')
-    op.add_argument("--window-size=1440x1024")
+    op.add_argument("--window-size=1920x1080")
     chromedriver = '/Users/maliboo/Downloads/chromedriver'
     driver = webdriver.Chrome(chromedriver,options=op)
     driver.get(message)
-    driver.save_screenshot(driver.title+".png")
+    filename = driver.title+ ".png"
+    driver.save_screenshot(filename)
     driver.quit()
 
+    return filename
 
-screen_f(
-    message = input('enter: ')
-)
