37f92af684a2cb422b41211038db5fdc8edced7b
dimasikjitss <57629224+dimasikjitss@users.noreply.github.com>
2020-07-10 10:19:05 +0600

    Update teleg-bot.py

--- teleg-bot.py
+++ teleg-bot.py
@@ -15,7 +15,7 @@ def send_screenchot(message):
     if not validators.url(message.text):
         bot.send_message(message.chat.id, "Не правильно введенный формат, введите повторно в формате https://www.google.ru/")
     else:
-        s = screen.screen_f(message.text) 
-        bot.send_photo(message.chat.id, photo=open(s,'rb'))
+        s = open(screen.screen_f(message.text),"rb") 
+        bot.send_document(message.chat.id,s)
 
 bot.polling(none_stop=True, interval=0)
